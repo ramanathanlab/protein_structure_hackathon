93cdccf11bc8ecf1428df486c7b9a83f9a5ca9bf
Kyle Hippe <kyle.hippe@gmail.com>
2022-11-29 12:36:13 -0600

    Forgot to add yesterday, attempt at pairwise

--- tm-align/tm-align.py
+++ tm-align/tm-align.py
@@ -1,10 +1,12 @@
-import subprocess
-from pathlib import Path
-from tqdm import tqdm
+import os
+import pickle
 import re
-from concurrent.futures import ProcessPoolExecutor
+import subprocess
+from argparse import ArgumentParser
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from functools import partial
-import json
+from pathlib import Path
+from typing import List, Tuple, Union
 
 """
 This script takes a list of pdbs and compares them to two reference pdbs.
@@ -14,38 +16,82 @@ This can be easily adapted to run pairwise tm-scores.
 Contact Kyle Hippe khippe@anl.gov with questions
 """
 
+PathLike = Union[Path, str]
 
-def run_tmalign(reference, pdb_1, pattern) -> float:
-    cmd = f"tmalign {str(pdb_1)} {str(reference)}"
+node_rank = int(os.environ.get("NODE_RANK", 0))  # zero indexed
+num_nodes = int(os.environ.get("NRANKS", 1))
+
+
+def run_tmalign(pdb1: Path, pdb2: Path, pattern: re.Pattern) -> float:
+    cmd = f"tmalign {str(pdb1)} {str(pdb2)}"
     res = subprocess.run(cmd.split(), capture_output=True)
-    score = float(pattern.findall(res.stdout.decode("utf-8"))[-1])
+    # Score 1 is normalized by pdb 2 score 2 is normalized by pdb 1
+    scores = pattern.findall(res.stdout.decode("utf-8"))
 
-    return pdb_1, score
+    return {(pdb1, pdb2): float(scores[0]), (pdb2, pdb1): float(scores[-1])}
 
 
-def process_against_reference(isoform_path, all_pdbs, out_path):
+def pairwise_processing(process_pdbs: List[Tuple[PathLike]], out_file: Path):
     pattern = re.compile("TM-score= ([+-]?[0-9]*[.]?[0-9]+)")
 
-    isoform_tmalign = partial(run_tmalign, isoform_path, pattern=pattern)
-    isoform_scores = {}
+    pairwise_tmalign = partial(run_tmalign, pattern=pattern)
+    futures = []
     with ProcessPoolExecutor() as pool:
-        for file, score in tqdm(
-            pool.map(isoform_tmalign, all_pdbs), total=len(all_pdbs)
-        ):
-            isoform_scores[str(file)] = score
+        for pair in process_pdbs:
+            futures.append(pool.submit(pairwise_tmalign, *pair))
 
-    with open(out_path, "w") as f:
-        json.dump(isoform_scores, f)
+    scores = []
+    for fut in as_completed(futures):
+        scores.append(fut.result())
+
+    pickle.dump(scores, out_file.open("wb"))
 
 
 if __name__ == "__main__":
 
-    tmscore_path = Path("/Users/kyle/Desktop/temp/mdh_visualization/tmscore/TMscore")
-    mdh_pdbs_path = Path("/Users/kyle/Desktop/temp/mdh_visualization/mdh_pdbs")
-    isoform_1_path = Path("/Users/kyle/Desktop/temp/mdh_visualization/2pwz.pdb")
-    isoform_2_path = Path("/Users/kyle/Desktop/temp/mdh_visualization/1nxu.pdb")
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-i", "--input_dir", type=Path, required=True, help="Folder with PDBs"
+    )
+    parser.add_argument(
+        "-o", "--out_dir", type=Path, required=True, help="Path to save chunks to"
+    )
 
-    all_pdbs = list(mdh_pdbs_path.glob("*.pdb"))
+    parser.add_argument(
+        "-g",
+        "--glob_pattern",
+        default="*.pbd",
+        help="Glob pattern to find input pdbs in folder",
+    )
 
-    process_against_reference(isoform_1_path, all_pdbs, "2wpz_tmalign_scores.json")
-    process_against_reference(isoform_2_path, all_pdbs, "1nxu_tmalign_scores.json")
+    args = parser.parse_args()
+
+    pdbs = list(args.input_dir.glob(args.glob_pattern))
+
+    combinations = []
+    for i in range(len(pdbs)):
+        for j in range(i, len(pdbs)):
+            combinations.append((pdbs[i], pdbs[j]))
+
+    print(len(combinations))
+    exit()
+    if num_nodes > 1:
+        chunk_size = max(len(combinations) // num_nodes, 1)
+        start_idx = node_rank * chunk_size
+        end_idx = start_idx + chunk_size
+        if node_rank + 1 == num_nodes:
+            end_idx = len(combinations)
+
+        print(
+            f"Node {node_rank} / {num_nodes} starting at {start_idx}, ending at {end_idx} ({len(combinations)=})"
+        )
+        node_data = combinations[start_idx:end_idx]
+        file_idx = f"_{start_idx}-{end_idx}"
+    else:
+        node_data = combinations[:]
+        file_idx = ""
+
+    args.out_dir.mkdir(exist_ok=True, parents=True)
+    out_file = args.out_dir / f"tmscore_combinations{file_idx}.pkl"
+
+    pairwise_processing(node_data, out_file)
